2fffbd3c7c62ee91a2d764d446d7b96f491c09da
rodgers-munene <munenerodgers72@gmail.com>
2025-06-21 01:41:02 +0300

    event registration controllers

--- backend/controllers/RegistrationController.js
+++ backend/controllers/RegistrationController.js
@@ -1,19 +1,89 @@
-const db = require('../config/db')
+const db = require("../config/db");
 
-const getRegisteredEvents = async (req, res, next) => {
+const getUserRegisteredEvents = async (req, res, next) => {
+    const userId = req.params.user_id
+    const { status } = req.query
+    try {
+        const [userEvents] = await db.query('SELECT * FROM registrations WHERE user_id = ? ', [userId])
+        if(userEvents.length === 0){
+            return res.status(200).json({message: "You have not registered for any event"})
+        }
 
-}
+        // if registered join the registration table and events table
+        let sql = `
+        SELECT events.*, registrations.status
+        FROM registrations
+        JOIN events ON registrations.event_id = events.id
+        WHERE registrations.user_id = ?`
 
-const allRegistered = async (req, res, next )=> {
+        const values = [userId]
 
-}
+        if(status){
+            sql += ' AND registrations.status = ?'
+            values.push(status)
+        }
 
-const registerForEvent = async(req, res, next) => {
+        const [registeredEvents] = await db.query(sql, values)
 
-}
+        res.status(200).json(registeredEvents)
 
-const cancelRegistration = async (req, res, next) => {
+    } catch (error) {
+        next(error)
+    }
+};
 
-}
+// get all registered users for an event
+const allRegisteredUsers = async (req, res, next) => {
+  const eventId = req.params.eventId;
+  const { status } = req.query
 
-module.exports = {getRegisteredEvents, allRegistered, registerForEvent, cancelRegistration}
+  try {
+    // first check if event exist
+    const [eventAvailable] = await db.query(
+      "SELECT * FROM events WHERE id = ?",
+      [eventId]
+    );
+    if (eventAvailable.length === 0) {
+      res.status(404);
+      throw new Error("Event not Found");
+    }
+
+    // query the registration data db to check all users registered for the specific event
+    // use join to combine the registration table and users table
+
+    let sql = `
+      SELECT users.id, users.user_name, registrations.status
+      FROM registrations
+      JOIN users ON registrations.user_id = users.id
+      WHERE registrations.event_id = ?
+    `;
+    const values = [eventId]
+
+    // filter by status it's provided
+    if(status){
+        sql += ' AND registrations.status = ?'
+        values.push(status)
+    }
+
+    // execute the query
+    const [registeredUsers] = await db.query(sql, values);
+    
+    if(registeredUsers.length === 0){
+        return res.status(200).json({message: "No users registered for the event."})
+    }
+    res.status(200).json(registeredUsers);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const registerForEvent = async (req, res, next) => {};
+
+const cancelRegistration = async (req, res, next) => {};
+
+module.exports = {
+  getUserRegisteredEvents,
+  allRegisteredUsers,
+  registerForEvent,
+  cancelRegistration,
+};
--- backend/routes/RegisterRoutes.js
+++ backend/routes/RegisterRoutes.js
@@ -1,14 +1,12 @@
-const { getAllEvents } = require('../controllers/EventController')
-const { allRegistered, cancelRegistration, registerForEvent } = require('../controllers/RegistrationController')
-
+const { allRegisteredUsers, cancelRegistration, registerForEvent, getUserRegisteredEvents } = require('../controllers/RegistrationController')
 const router = require('express').Router()
 
 
 // GET all users registered for a specific event
-router.get('/:eventId/registrations', allRegistered);
+router.get('/:eventId/registrations', allRegisteredUsers);
 
 // GET all events a specific user has registered for
-router.get('/users/:userId/registrations', getAllEvents);
+router.get('/users/:userId/registrations', getUserRegisteredEvents);
 
 // POST a new registration (user registers for an event)
 router.post('/registrations', registerForEvent);
